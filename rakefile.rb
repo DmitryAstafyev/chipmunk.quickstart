68822eead5926563ee4c7210bdb8731b2e5553c3
dmitry.astafyev <dmitry.astafyev@esrlabs.com>
2020-03-03 10:02:07 +0100

    [](feat) remove debug stuff

--- rakefile.rb
+++ rakefile.rb
@@ -6,25 +6,6 @@ require './scripts/tools'
 PLUGINS_DEST_FOLDER = "./plugins";
 PLUGIN_RELEASE_FOLDER = "./releases"
 
-task :build3 do
-    success = true
-    register = Register.new()
-    versions = Versions.new()
-    loop do
-        plugin_info = register.next()
-        if plugin_info == nil
-            break
-        end
-        plugin = Plugin.new(plugin_info['name'], plugin_info['repo'], PLUGINS_DEST_FOLDER, plugin_info['version'], versions.get(), versions.get_hash())
-        if plugin.build()
-            puts "Plugin #{plugin_info['name']} is built SUCCESSFULLY"
-        else
-            success = false
-            puts "Fail to build plugin #{plugin_info['name']}"
-        end
-    end
-end
-
 task :build, [:target] do |t, args|
     if args.target == nil
         putsAccent("Please define target to be built like: \"rake build[./plugins/plugin.complex]\"", true)
@@ -39,10 +20,4 @@ task :build, [:target] do |t, args|
     end
     # ...
     puts "Traget is: #{args.target}"
-end
-
-
-
-task :test do
-    puts File.basename('/d/t')
 end
